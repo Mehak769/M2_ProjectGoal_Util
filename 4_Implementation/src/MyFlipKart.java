f3a8811dd57d876c1bbaa4d3cbea3e8498043771
Mehak <mehak1815719@gmail.com>
2022-03-10 17:12:52 +0530

    commit

--- 4_Implementation/src/MyFlipKart.java
+++ 4_Implementation/src/MyFlipKart.java
@@ -9,117 +9,117 @@ public class MyFlipKart {
     }
 
     public static void main(String[] args) {
-        // -------Data generation code ---- START ------------------------------//
+        
 
-        // Creating Customer --> Piyush
-        Customer piyush = new Customer("Piyush Khandelwal","9876987655");
+        
+        Customer mehak = new Customer("Mehak sharma","8219663373");
 
-        // Creating Seller --> Daily Needs Groceries
-        Seller dailyNeedsGrocery = new Seller("Daily Needs Groceries","9876543210");
+        
+        Seller dailyNeedsGrocery = new Seller("Daily Needs Groceries","8219663373");
 
-        // Creating Seller --> Fashion Point
-        Seller fashionPoint = new Seller("Fashion Point","9988776655");
+        
+        Seller fashionPoint = new Seller("Fashion Point","8219663373");
 
-        // Creating Seller --> Digi Electronics
-        Seller digiElectronics = new Seller("Digi Electronics","9876598765");
+        
+        Seller digiElectronics = new Seller("Digi Electronics","8219663373");
 
-        // Creating category --> Electronics
+        
         Category electronics = new Category("Electronics");
 
-        // Creating category --> Cosmetics
+        
         Category cosmetics = new Category("Cosmetics");
 
-        // Creating category --> Grocery
+        
         Category grocery = new Category("Groceries");
 
-        // Creating category --> Clothing
+        
         Category clothing = new Category("Clothing");
 
-        // -------Data generation code ---- END ------------------------------//
+        
 
         MyFlipKart myFlipKart = new MyFlipKart();
 
-        // Adding all the categories to Flipkart catalog
+        
         myFlipKart.productsCatalog.addCategory(clothing);
         myFlipKart.productsCatalog.addCategory(grocery);
         myFlipKart.productsCatalog.addCategory(cosmetics);
         myFlipKart.productsCatalog.addCategory(electronics);
 
-        // Seller Digi Electronics adding Mobile to myFlipKart catalog
+    
         digiElectronics.registerProduct(myFlipKart.productsCatalog,
         new Product("Mobile","Latest Technology",10000.00,electronics,5));
 
-        // Seller Digi Electronics adding Camera to myFlipKart catalog
-        digiElectronics.registerProduct(myFlipKart.productsCatalog, //
+        
+        digiElectronics.registerProduct(myFlipKart.productsCatalog, 
         new Product("Camera","Advanced Technology",50000.00,electronics,10));
 
-        // Seller FashionPoint adding Wearbles to myFlipKart catalog
-        fashionPoint.registerProduct(myFlipKart.productsCatalog, //
+        
+        fashionPoint.registerProduct(myFlipKart.productsCatalog, 
         new Product("Mens Jackets","XL - Size",1000.00,clothing,10));
         
-        fashionPoint.registerProduct(myFlipKart.productsCatalog, //
+        fashionPoint.registerProduct(myFlipKart.productsCatalog, 
         new Product("Jackets","XL - Size",1000.00,clothing,10));
         
-        // Seller FashionPoint adding Cosmetics to myFlipKart catalog
-        fashionPoint.registerProduct(myFlipKart.productsCatalog, // 
+        
+        fashionPoint.registerProduct(myFlipKart.productsCatalog, 
         new Product("Nail Paint","Red Color",500.00,cosmetics,25));
 
-        // Seller dailyNeedsGrocery adding grocery to myFlipKart catalog
-        dailyNeedsGrocery.registerProduct(myFlipKart.productsCatalog, //
+        
+        dailyNeedsGrocery.registerProduct(myFlipKart.productsCatalog, 
         new Product("Sugar","Fine quality",40.00,grocery,1000));
 
-        // Seller dailyNeedsGrocery adding grocery to myFlipKart catalog
-        dailyNeedsGrocery.registerProduct(myFlipKart.productsCatalog, //
+        
+        dailyNeedsGrocery.registerProduct(myFlipKart.productsCatalog, 
         new Product("Milk","100% Pure",50.00,grocery,250));
         
-        dailyNeedsGrocery.registerProduct(myFlipKart.productsCatalog, //
+        dailyNeedsGrocery.registerProduct(myFlipKart.productsCatalog, 
         new Product("Toned Milk","Hygenic and Pure",45.00,grocery,250));
         
-        dailyNeedsGrocery.registerProduct(myFlipKart.productsCatalog, //
+        dailyNeedsGrocery.registerProduct(myFlipKart.productsCatalog, 
         new Product("Milk Cream","Natural",145.00,grocery,100));
 
 
-        // Customer searching for product --> milk
+        
         ArrayList<Product> milkResults = myFlipKart.productsCatalog.searchProduct("Milk");
-        //System.out.println(milkResults);
+    
 
-        // Customer adding 10 packets of milk to cart
+        
         Item milkTenPackets = new Item(milkResults.get(0),10);
-        piyush.addItemToCart(milkTenPackets);
+        mehak.addItemToCart(milkTenPackets);
 
-        // Printing current cart status
-        piyush.printCartItems();
+        
+        mehak.printCartItems();
 
-        // Customer searching for another product --> camera
+        
         ArrayList<Product> cameraResults = myFlipKart.productsCatalog.searchProduct("CaMeRa");
-        //System.out.println(cameraResults);
+        
 
-        // Customer adding 1 camera to cart
+        
         Item oneCamera = new Item(cameraResults.get(0),1);
-        piyush.addItemToCart(oneCamera);
+        mehak.addItemToCart(oneCamera);
 
-        // Printing current cart status
-        piyush.printCartItems();
+        
+        mehak.printCartItems();
 
-        // Customer updating quantity of milk packets from 10 to 15
-        piyush.updateItemCount(milkTenPackets,15);
+        
+        mehak.updateItemCount(milkTenPackets,15);
 
-        // Printing current cart status
-        piyush.printCartItems();
+        
+        mehak.printCartItems();
 
-        // Customer placing the order
-        piyush.placeOrder();
+        
+        mehak.placeOrder();
 
-        // Customer cart is empty after checkout
-        piyush.printCartItems();
+        
+        mehak.printCartItems();
 
-        // Printing the current Order of customer
-        System.out.println(piyush.getCurrentOrder());
+        
+        System.out.println(mehak.getCurrentOrder());
 
-        // Customer order is moved to shipment
-        Shipment s = piyush.getCurrentOrder().moveToShipment();
+        
+        Shipment s = mehak.getCurrentOrder().moveToShipment();
 
-        // Printing the shipment details
+        
         System.out.println(s);
 
 
